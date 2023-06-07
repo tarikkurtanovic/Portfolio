454f94e0afc870f001602196aee38c892d23a380
tarikkurtanovic <tarik2371@gmail.com>
2023-06-07 19:31:52 +0200

    responsive design done

---
+++ script.js
@@ -0,0 +1,10 @@
+const sidemenu = document.getElementById("sidemenu");
+const menu = document.querySelector(".bx-x");
+function openmenu() {
+  sidemenu.style.right = "0";
+  menu.classList.add(".hidden");
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
